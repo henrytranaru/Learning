b6f60b70f6f6cd11e2c7837d6d216cc5348c6484
henrytranaru <76545378+henrytranaru@users.noreply.github.com>
2023-07-19 20:03:58 -0400

    CREATE TABLE INSERT TO.sql

--- CREATE TABLE INSERT TO.sql
+++ CREATE TABLE INSERT TO.sql
@@ -2,38 +2,38 @@
 CREATE TABLES, INSERT TABLES
 */
 
---CREATE TABLE EmployeeDemographics
---(
---EmployeeID int,
---FirstName varchar(50),
---LastName varchar (50),
---Age int,
---Gender varchar(50)
---)
+CREATE TABLE EmployeeDemographics
+(
+EmployeeID int,
+FirstName varchar(50),
+LastName varchar (50),
+Age int,
+Gender varchar(50)
+)
 
---CREATE TABLE EmployeeSalary
---(
---EmployeeID int,
---JobTitle varchar(50),
---Salary int
---)
+CREATE TABLE EmployeeSalary
+(
+EmployeeID int,
+JobTitle varchar(50),
+Salary int
+)
 
---INSERT INTO EmployeeDemographics VALUES
---(1011, 'Ryan', 'Howard', 26, 'Male'),
---(NULL,'Holly', 'Flax', NULL, NULL ),
---(1013, 'Darryl', 'Philbin', NULL, 'Male'),
---(NULL, NULL, NULL, NULL, NULL),
---(NULL, NULL, NULL, NULL, NULL)
+INSERT INTO EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL,'Holly', 'Flax', NULL, NULL ),
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(NULL, NULL, NULL, NULL, NULL),
+(NULL, NULL, NULL, NULL, NULL)
 
---SELECT *
---FROM EmployeeDemographics
+SELECT *
+FROM EmployeeDemographics
 
---INSERT INTO EmployeeSalary VALUES
---(NULL, NULL, NULL),
---(NULL, NULL, NULL),
---(NULL, NULL, NULL),
---(1010, NULL, 47000),
---(NULL, 'Salesman', 43000)
+INSERT INTO EmployeeSalary VALUES
+(NULL, NULL, NULL),
+(NULL, NULL, NULL),
+(NULL, NULL, NULL),
+(1010, NULL, 47000),
+(NULL, 'Salesman', 43000)
 
---SELECT *
---FROM EmployeeSalary
+SELECT *
+FROM EmployeeSalary
