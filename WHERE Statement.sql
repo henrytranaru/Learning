6d6c681e5d5aa7aa25b863a6094050e3bda47a22
henrytranaru <76545378+henrytranaru@users.noreply.github.com>
2023-07-19 20:05:41 -0400

    Add files via upload

---
+++ WHERE Statement.sql
@@ -0,0 +1,35 @@
+/*
+WHERE Statement
+=, <>, <, >, AND, OR, LIKE, NULL, NOT NULL, IN
+*/
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE AGE <= 32 AND Gender = 'Male'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE AGE <= 32 OR Gender = 'Male'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE LastName LIKE '%S%'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE LastName LIKE 'S%o%'
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName is NULL
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName IS NOT NULL
+
+
+--- IN is like multiple equal statements
+
+--SELECT *
+--FROM EmployeeDemographics
+--WHERE FirstName IN ('Jim', 'Michael')
